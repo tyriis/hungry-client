4ca71af6865d37edc3a344f3f6cd4aa868d05ac2
nils biesalski <nils.biesalski@gmail.com>
2014-12-20 12:38:07 +0100

    implemented prev week, current week functionality

--- js/app/controller/calendar.js
+++ js/app/controller/calendar.js
@@ -3,8 +3,17 @@ define('app/controller/calendar', ['app/data', 'lodash'], function(data, _) {
     var CalendarController = function($scope, $filter) {
         $scope.days = data.getWeekDays();
         $scope.next = function() {
-            console.log('we are here');
-            $scope.days = data.getWeekDays(_.last($scope.days));
+            var day = _.clone(_.last($scope.days));
+            day.setDate(day.getDate() + 1);
+            $scope.days = data.getWeekDays(day);
+        };
+        $scope.prev = function() {
+            var day = _.clone(_.first($scope.days));
+            day.setDate(day.getDate() - 7);
+            $scope.days = data.getWeekDays(day);
+        };
+        $scope.current = function() {
+            $scope.days = data.getWeekDays();
         };
         $scope.isToday = function(day) {
             return $filter('date')(new Date(), 'fullDate') === $filter('date')(day, 'fullDate');
--- js/app/data.js
+++ js/app/data.js
@@ -1,9 +1,6 @@
 define('app/data', ['lodash'], function(_) {
     return {
         getWeekDays: function(from) {
-            if (from !== undefined) {
-                from.setDate(from.getDate() + 1)
-            }
             var days = [from || new Date()];
             while (days.length < 7) {
                 var day = _.clone(days[days.length - 1]);
